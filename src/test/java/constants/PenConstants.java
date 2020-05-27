6d4cce76a0aac47a228553cf27ab6dd9e087d950
Lida <lion.daredevil@gmail.com>
2020-05-27 08:30:14 +0200

    added unit tests for pen class

---
+++ src/test/java/constants/PenConstants.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class PenConstants {
+    public static final int BASE_INK_CONTAINER_VALUE = 10;
+    public static final double BASE_SIZE_LETTER = 1.0;
+    public static final String BASE_WORD = "1234567890";
+    public static final String BASE_COLOR = "BLUE";
+    public static final String BASE_WORD_WITH_SPACE = "1 2 3 4 5 6 7 8 9 0";
+
+}
